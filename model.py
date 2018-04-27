9be2fa70426fa0e5a4892900570f8e086abfbff2
Neo.Li <limigyao@ainirobot.com>
2018-04-27 16:56:12 +0800

    few lines in model

---
+++ model.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+class DarkNet19(nn.Module):
+    def __init__(self,):
+        super(DarkNet19,self).__init__()
+        self.block1 = nn.Sequential(
+
+    def forward(self, x):
+        pass
